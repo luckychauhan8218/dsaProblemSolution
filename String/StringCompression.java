3b5956fff070c1f55136936c06e492c184d53ef5
Lucky Chauhan <159449655+luckychauhan8218@users.noreply.github.com>
2026-03-07 10:12:31 +0530

    String Compression abbcccccaaa into a1b2c5a3

---
+++ String/StringCompression.java
@@ -0,0 +1,17 @@
+class StringCompression {
+    public static void main(String[] args) {
+        String str="abbcccccaaa";
+        String result="";
+        int count=1;
+        for(int i=0;i<str.length();i++){
+            if(i<str.length()-1&&str.charAt(i)==str.charAt(i+1)){
+                count++;
+            }
+            else{
+                result=result+str.charAt(i)+count;
+                count=1;
+            }
+        }
+        System.out.print(result);
+    }
+}
